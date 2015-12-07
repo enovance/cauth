d48b3501d4b1e8b5a8e1f9f28a0a5a9c578ba57d
George Peristerakis <gperiste@redhat.com>
2015-12-07 09:19:44 -0500

    Added error logging to url requests
    Story #1124
    
    Change-Id: Iead2b976cab14550aa8c634b606a8fdc378130d2

--- cauth/tests/test_service_plugins.py
+++ cauth/tests/test_service_plugins.py
@@ -34,6 +34,7 @@ class TestGerritPlugin(TestCase):
         self.assertIn('data', kwargs)
         self.assertIn('auth', kwargs)
         self.key_amount_added += 1
+        return FakeResponse(200)
 
     def gerrit_get_account_id_mock(self, *args, **kwargs):
         data = json.dumps({'_account_id': 42})
@@ -109,7 +110,7 @@ class TestGerritPlugin(TestCase):
                                    'ssh_keys': []})
             self.assertEqual(True, ger.add_account_as_external.called)
         with patch('cauth.service.gerrit.requests') as r:
-            r.put = lambda *args, **kwargs: None
+            r.put = lambda *args, **kwargs: FakeResponse(200)
             r.get = self.gerrit_get_account_id_mock2
             ger.add_account_as_external = Mock()
             ger.register_new_user({'login': 'john',
